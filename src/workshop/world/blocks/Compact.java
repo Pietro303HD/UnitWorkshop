09424d90380f8ac771ada2baca8bea7201de8087
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2022-02-01 20:04:43 -0300

    Create Compact.java

---
+++ src/workshop/world/blocks/Compact.java
@@ -0,0 +1,31 @@
+package workshop.world.blocks;
+
+import arc.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.util.*;
+import mindustry.entities.bullet.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.meta.*;
+
+public class Compact extends Block{
+    public BuildUnitPayload(String name){
+        super(name);
+
+        buildVisibility = BuildVisibility.sandboxOnly;
+        category = Category.units;
+    }
+
+    @Override
+    public boolean canBeBuilt(){
+        return false;
+    }
+
+    public class CompactBuild extends Building{
+    }
+}
