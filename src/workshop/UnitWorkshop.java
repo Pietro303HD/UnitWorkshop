acf0e18e5f1e34c31f062f37e6a2cb9450edadd2
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2022-02-01 18:17:46 -0300

    woo

--- src/workshop/UnitWorkshop.java
+++ src/workshop/UnitWorkshop.java
@@ -1,1 +1,17 @@
+package workshop;
 
+import arc.util.*;
+import mindustry.mod.*
+
+public class UnitWorkshop extends Mod{
+
+    public UnitWorkshop(){
+        Log.info("Constructor has been called.");
+    }
+
+    @Override
+    public void loadContent(){
+        Log.info("Loading no content, for now.");
+    }
+
+}
