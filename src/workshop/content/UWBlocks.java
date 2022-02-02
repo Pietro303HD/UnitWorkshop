f2112774941c0d47682ed91edb42c2ae6b955d80
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2022-02-02 08:51:18 -0300

    hmm

--- src/workshop/content/UWBlocks.java
+++ src/workshop/content/UWBlocks.java
@@ -3,6 +3,7 @@ package workshop.content;
 import mindustry.ctype.*;
 import mindustry.world.*;
 import workshop.world.blocks.*;
+import workshop.type.*;
 
 public class UWBlocks implements ContentList{
     public static Block unitCrate, workshop;
@@ -12,6 +13,9 @@ public class UWBlocks implements ContentList{
         unitCrate = new CompactPayload("unit-crate");
         workshop = new Workshop("workshop"){{
             output = unitCrate;
+            recipes = [
+                new WeaponRecipe(ItemStack.with(Item.copper, 50), UnitTypes.scepter.weapons.get(0))
+            ];
         }};
     }
 }
