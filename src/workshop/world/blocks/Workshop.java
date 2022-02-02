5aee79468dbc9ac183e223dee54b1a158a8dfe78
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2022-02-01 22:27:15 -0300

    go on

--- src/workshop/world/blocks/Workshop.java
+++ src/workshop/world/blocks/Workshop.java
@@ -1,7 +1,18 @@
 package workshop.world.blocks;
 
 import mindustry.world.*;
+import mindustry.world.blocks.payloads.*;
 import mindustry.gen.*;
 
-public class Workshop extends Block{
+public class Workshop extends PayloadBlock{
+    public Workshop(String name){
+        super(name);
+
+        buildVisibility = BuildVisibility.shown;
+        category = Category.units;
+        configurable = true;
+    }
+
+    public class WorkshopBuild extends PayloadBuild{
+    }
 }
