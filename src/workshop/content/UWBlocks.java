e0d01b6c695854ac5fad41eb1380a8d128581c2e
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2022-02-02 09:02:54 -0300

    thanks ship!

--- src/workshop/content/UWBlocks.java
+++ src/workshop/content/UWBlocks.java
@@ -14,9 +14,10 @@ public class UWBlocks implements ContentList{
         unitCrate = new CompactPayload("unit-crate");
         workshop = new Workshop("workshop"){{
             output = unitCrate;
-            recipes = [
-                new WeaponRecipe(ItemStack.with(Items.copper, 50, Items.silicon, 100), UnitTypes.scepter.weapons.get(0))
-            ];
+            recipes = new WeaponRecipe[]{
+                new WeaponRecipe(ItemStack.with(Items.copper, 50, Items.silicon, 100), UnitTypes.scepter.weapons.get(0)),
+                new WeaponRecipe(ItemStack.with(Items.lead, 200, Items.graphite, 70, Items.silicon, 120), UnitTypes.retusa.weapons.get(0))
+            };
         }};
     }
 }
