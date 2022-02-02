d2258b728301149c0023020d08902e0a8082abe6
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2022-02-02 17:33:33 -0300

    pleasee

---
+++ src/workshop/world/blocks/TestBlock.java
@@ -0,0 +1,39 @@
+package workshop.world.blocks;
+
+import arc.scene.ui.layout.*;
+import arc.scene.style.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.gen.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.meta.*;
+import mindustry.world.blocks.payloads.*;
+import workshop.world.blocks.*;
+import workshop.type.*;
+
+public class TestBlock extends PayloadBlock{
+    public TestBlock(String name){
+        super(name);
+
+        size = 5;
+        outputsPayload = true;
+        rotate = true;
+        update = true;
+        buildVisibility = BuildVisibility.shown;
+        category = Category.units;
+        configurable = true;
+    }
+
+    public class TestBuild extends PayloadBlockBuild<BuildPayload>{
+
+        @Override
+        public void buildConfiguration(Table table){
+            table.button(Icon.download, () -> {
+                payload = new BuildPayload(Vars.content.blocks.router, team)
+            });           
+        }
+
+    }
+}
