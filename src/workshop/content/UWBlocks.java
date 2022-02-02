2e70e6079be4f8072abcdc7f9a016c8f74f588a5
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2022-02-01 23:33:28 -0300

    Update UWBlocks.java

--- src/workshop/content/UWBlocks.java
+++ src/workshop/content/UWBlocks.java
@@ -5,10 +5,13 @@ import mindustry.world.*;
 import workshop.world.blocks.*;
 
 public class UWBlocks implements ContentList{
-    public static Block unitCrate;
+    public static Block unitCrate, workshop;
 
     @Override
     public void load() {
         unitCrate = new CompactPayload("unit-crate");
+        workshop = new Workshop("workshop"){{
+            output = unitCrate;
+        }};
     }
 }
