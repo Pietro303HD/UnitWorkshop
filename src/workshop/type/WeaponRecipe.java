56f9f6bf49579febf2ff42b8a089e8e85cb7673f
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2022-02-02 09:10:32 -0300

    Update WeaponRecipe.java

--- src/workshop/type/WeaponRecipe.java
+++ src/workshop/type/WeaponRecipe.java
@@ -5,4 +5,9 @@ import mindustry.type.*;
 public class WeaponRecipe{
     public Weapon weapon;
     public ItemStack[] requirements;
+
+    public WeaponRecipe(ItemStack[] items, Weapon weapon){
+        this.weapon = weapon;
+        this.requirements = items;
+    }
 }
