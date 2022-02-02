27b6ad4d27f9444ad08b17c33c57ffc04a7a5c51
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2022-02-01 21:29:09 -0300

    and.. done!

--- src/workshop/content/UWBlocks.java
+++ src/workshop/content/UWBlocks.java
@@ -1,4 +1,13 @@
 package workshop.content;
 
+import mindustry.ctype.*;
+import workshop.world.blocks.*;
+
 public class UWBlocks extends ContentList{
+    public static Block unitCrate;
+
+    @Override
+    public void load() {
+        unitCrate = new CompactPayload("unit-crate");
+    }
 }
